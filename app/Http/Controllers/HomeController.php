c02de07e804d4017c5b475964c7eabd35d7014ef
ruchdir <ruchdir@bfsasia.com>
2022-07-06 17:13:16 +0700

    Update Show All Post When not have Category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,9 @@ class HomeController extends Controller
     //
     public function index(){
         $categories = Category::all();
-        $posts = Post::where('category_id', request('category_id'))
+        $posts = Post::when(request('category_id'), function($query){
+            $query->where('category_id', request('category_id'));
+        })
             ->latest()
             ->get();
         
