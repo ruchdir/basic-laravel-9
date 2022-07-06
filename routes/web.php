3e42dd77840f0ab0d55774ba65a2ea97c267b4af
ruchdir <ruchdir@bfsasia.com>
2022-07-06 10:29:47 +0700

    Add Route Name

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', view:'index');
-Route::view('/about', view:'about');
-Route::view('/contact', view:'contact');
+Route::view('/', view:'index')->name('home');
+Route::view('/about', view:'about')->name('about');
+Route::view('/contact', view:'contact')->name('contact');
