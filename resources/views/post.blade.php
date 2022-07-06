531bb2a3569cf82f6c697c1be5ca0ba5d710a8c6
ruchdir <ruchdir@bfsasia.com>
2022-07-06 17:22:38 +0700

    Add Post Page

---
+++ resources/views/post.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- Page header with logo and tagline-->
+<header class="py-5 bg-light border-bottom mb-4">
+    <div class="container">
+        <div class="text-center my-5">
+            <h1 class="fw-bolder">{{ $post->title }}</h1>
+        </div>
+    </div>
+</header>
+<!-- Page content-->
+<div class="container">
+    <div class="row">
+        <!-- Blog entries-->
+        <div class="col-lg-12">
+            <div class="card-body">
+                <h2 class="card-title">{{ $post->post_text }}</h2>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
+                <a class="btn btn-primary" href="#!">Read more →</a>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+        
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('/about', view:'about')->name('about');
 Route::view('/contact', view:'contact')->name('contact');
+
+Route::get('/posts/{postId}', [PostController::class, 'show'])->name('posts.show');
